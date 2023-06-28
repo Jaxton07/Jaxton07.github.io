89977a7d62e3a057a9cf4585f3182d3f392de3bd
Jaxton07 <minghao321@foxmail.com>
2023-06-28 23:00:24 +0800

    Site updated: 2023-06-28 23:00:23

---
+++ js/pv_uv.js
@@ -0,0 +1,9 @@
+<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?c7c9e96fb65c43046e95a00f062bf641";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
